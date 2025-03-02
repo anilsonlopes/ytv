aaf205b326a8e06888e193f89c5187d6d57e30ba
Anilson Lopes <nissobmx@gmail.com>
2025-03-02 11:53:09 -0300

    🎉 initialize project with tailwind and typescript

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2 class="text-red-500">Hello World</h2>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["assets/css/main.css"],
+  modules: [],
+  future: {
+    compatibilityVersion: 4,
+  },
+  typescript: {
+    typeCheck: true,
+  },
+});
