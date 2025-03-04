b67220941c01131014ac7c18cdba423bddf6fa3f
Anilson Lopes <nissobmx@gmail.com>
2025-03-03 21:23:11 -0300

    ✨ enhance MoviesTop10OnTvThisWeek component with swiper for dynamic movie display; update layout and add navigation link

--- app/components/movies/MoviesTop10OnTvThisWeek.vue
+++ app/components/movies/MoviesTop10OnTvThisWeek.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>MoviesTop10OnTvThisWeek</div>
+  <div class="py-10">
+    <div class="flex justify-between items-end mb-6">
+      <h4 class="text-2xl font-semibold">Top 10 no Quila Tv essa semana</h4>
+      <nuxt-link to="#" class="flex items-center space-x-3">
+        <span class="inline-block">Ver mais</span>
+        <Icon name="solar:arrow-right-line-duotone" class="-mb-px" />
+      </nuxt-link>
+    </div>
+    <ClientOnly>
+      <swiper-container :slides-per-view="'auto'" :space-between="25">
+        <swiper-slide
+          v-for="movie in movies.all"
+          class="h-72 w-52"
+          :key="movie.id"
+        >
+          <img
+            :src="movie.poster"
+            class="h-72 w-52 object-cover"
+            :alt="movie.title"
+          />
+        </swiper-slide>
+        <div class="swiper-pagination"></div>
+      </swiper-container>
+    </ClientOnly>
+  </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
