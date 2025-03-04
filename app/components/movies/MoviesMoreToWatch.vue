bea6254e9a614c4871f9d07c7b75909842847835
Anilson Lopes <nissobmx@gmail.com>
2025-03-04 11:16:41 -0300

    ✨ enhance layout of MoviesMoreToWatch, MoviesTop10OnTvThisWeek, MoviesTvFavourites, and MoviesWhatToWatchThisWeek components for improved presentation; add show-info prop to MadPoster in MoviesTvFavourites

--- app/components/movies/MoviesMoreToWatch.vue
+++ app/components/movies/MoviesMoreToWatch.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
     <h5 class="text-center text-4xl font-bold">Mais para assistir</h5>
-    <p class="text-center mt-3">
+    <p class="text-center mt-2">
       <span class="text-red-400">Melhores escolhas:</span> programas de TV e
       filmes só para você
     </p>
 
-    <div class="flex space-x-5">
-      <div v-for="movie in movies.all" :key="movie.id">
-        <MadPoster :movie="movie" />
+    <div class="py-10 space-y-5">
+      <div class="grid grid-cols-6 gap-5 -mx-10">
+        <div class="w-full bg-neutral-600 h-72"></div>
+        <div class="w-full bg-neutral-600 h-72"></div>
+        <div class="w-full bg-neutral-600 h-72"></div>
+        <div class="w-full bg-neutral-600 h-72"></div>
+        <div class="w-full bg-neutral-600 h-72"></div>
+        <div class="w-full bg-neutral-600 h-72"></div>
+      </div>
+      <div class="grid grid-cols-5 gap-5 -mx-10">
+        <div class="w-full bg-neutral-600 h-96"></div>
+        <div class="w-full bg-neutral-600 h-96"></div>
+        <div class="w-full bg-neutral-600 h-96"></div>
+        <div class="w-full bg-neutral-600 h-96"></div>
+        <div class="w-full bg-neutral-600 h-96"></div>
       </div>
     </div>
 
--- app/components/movies/MoviesTvFavourites.vue
+++ app/components/movies/MoviesTvFavourites.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-10">
+  <div class="py-10 -mx-10 md:mx-0">
     <div class="flex justify-between items-end mb-6">
       <h4 class="text-2xl font-semibold">Mais listados</h4>
       <nuxt-link to="#" class="flex items-center space-x-3">
@@ -10,7 +10,7 @@
     <ClientOnly>
       <swiper-container :slides-per-view="'auto'" :space-between="25">
         <swiper-slide v-for="movie in movies.all" class="w-52" :key="movie.id">
-          <MadPoster :movie="movie" />
+          <MadPoster :movie="movie" show-info />
         </swiper-slide>
       </swiper-container>
     </ClientOnly>
